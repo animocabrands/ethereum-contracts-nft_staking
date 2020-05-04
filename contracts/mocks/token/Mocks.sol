97e2579b3c6ba8d03d9a63b9a0a79c7170e0aab3
Vidhvat Madan <vidhvatm@animocabrands.com>
2020-05-04 11:54:17 +0800

    migration of staking contract

--- contracts/mocks/token/Mocks.sol
+++ contracts/mocks/token/Mocks.sol
@@ -1,4 +1,3 @@
-
 pragma solidity ^0.6.6;
 
 import "@animoca/ethereum-contracts-erc20_base/contracts/mocks/token/ERC20/ERC20FullMock.sol";
--- migrations/2_staking.js
+++ migrations/2_staking.js
@@ -1,4 +1,3 @@
-const { contract } = require('@openzeppelin/test-environment');
 const program = require('commander');
 const { NFCollectionMaskLength } = require('@animoca/f1dt-core_metadata').constants;
 const { BN } = require('@openzeppelin/test-helpers');
@@ -6,9 +5,9 @@ const DayInSeconds = 86400;
 const FreezePeriodSeconds = new BN(DayInSeconds);
 const DividendTokenInitialBalance = new BN("100000000000000000000000");
 const PayoutPeriodLength = new BN(7); // days
-const NftStaking = contract.fromArtifact("NftStaking");
-const AssetsInventoryMock = contract.fromArtifact("AssetsInventoryMock");
-const ERC20BaseMock = contract.fromArtifact("ERC20BaseMock");
+const NftStakingMock = artifacts.require("NftStakingMock");
+const AssetsInventoryMock = artifacts.require("AssetsInventoryMock");
+const ERC20FullMock = artifacts.require("ERC20FullMock");
 
 const CarRarities = {
     Common: 1,
@@ -33,14 +32,14 @@ module.exports = async (deployer, network, accounts) => {
         case "ganache":
             await deployer.deploy(AssetsInventoryMock,NFCollectionMaskLength);
             this.nftContract = await AssetsInventoryMock.deployed();
-            await deployer.deploy(ERC20BaseMock,DividendTokenInitialBalance);
-            this.dividendTokenContract = await ERC20BaseMock.deployed();
+            await deployer.deploy(ERC20FullMock,DividendTokenInitialBalance);
+            this.dividendTokenContract = await ERC20FullMock.deployed();
             break;
         case "rinkeby":
             const nftContractAddressRinkeby = program.nftContractAddressRinkeby;
             const ERC20BaseAddressRinkeby = program.ERC20BaseAddressRinkeby;
             this.nftContract = nftContractAddressRinkeby?await AssetsInventoryMock.at(nftContractAddressRinkeby):await AssetsInventoryMock.new(NFCollectionMaskLength);
-            this.dividendTokenContract = ERC20BaseAddressRinkeby? await ERC20BaseMock.at(ERC20BaseAddressRinkeby):await ERC20BaseMock.new(DividendTokenInitialBalance);
+            this.dividendTokenContract = ERC20BaseAddressRinkeby? await ERC20FullMock.at(ERC20BaseAddressRinkeby):await ERC20FullMock.new(DividendTokenInitialBalance);
             break;
         case "mainnet":
             
@@ -54,7 +53,7 @@ module.exports = async (deployer, network, accounts) => {
     console.log("Creating Staking Contract");
         
     console.log("Deploying Staking Contract");
-    const result = await deployer.deploy(NftStaking,
+    const result = await deployer.deploy(NftStakingMock,
         PayoutPeriodLength,
         FreezePeriodSeconds,
         this.nftContract.address,
@@ -62,7 +61,7 @@ module.exports = async (deployer, network, accounts) => {
         CarWeightsConfig.map(x => x.rarity),
         CarWeightsConfig.map(x => x.weight));
 
-    this.stakingContract = await NftStaking.deployed();
+    this.stakingContract = await NftStakingMock.deployed();
 
     await this.dividendTokenContract.transfer(this.stakingContract.address, DividendTokenInitialBalance);    
     

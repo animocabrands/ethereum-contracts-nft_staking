5d150cd61cbba0f21cd88f383163094bb165b28b
animocabrands <55173168+animocabrands@users.noreply.github.com>
2020-05-04 15:28:26 +0800

    V1.0.0
    * migration to solidity 0.6
    
    * add ERC721 non-safe transfer fallback on withdrawal
    
    * added migration script
    
    * migration of staking contract
    
    * fixed weights
    
    * Adding currentCycle in Deposit and Withdrawal events
    
    Co-authored-by: Nathan Sala <sala.nathan@gmail.com>
    Co-authored-by: Vidhvat Madan <vidhvatm@animocabrands.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,15 @@
 # Changelog
 
+## 1.0.0 (03/05/2020)
+
+### Breaking changes
+ * Migration to `@animoca/ethereum-contracts-core_library:1.0.0` with `solc:0.6.x` and `@openzeppelin/contracts:3.x`.
+
+### New features
+ * Added `NftStakingMock.sol` and `NftStakingTestableMock.sol`.
+
+### Improvements
+ * Better abstraction of core staking features.
+
 ## 0.0.1 (15/04/2020)
 * Initial commit.
---
+++ migrations/2_staking.js
@@ -0,0 +1,66 @@
+const program = require('commander');
+const { NFCollectionMaskLength } = require('@animoca/f1dt-core_metadata').constants;
+const { BN } = require('@openzeppelin/test-helpers');
+const DayInSeconds = 86400;
+const FreezePeriodSeconds = new BN(DayInSeconds);
+const DividendTokenInitialBalance = new BN("100000000000000000000000");
+const PayoutPeriodLength = new BN(7); // days
+const NftStakingMock = artifacts.require("NftStakingMock");
+const AssetsInventoryMock = artifacts.require("AssetsInventoryMock");
+const ERC20FullMock = artifacts.require("ERC20FullMock");
+
+const CarRarities = {
+    Common: 1,
+    Rare: 2,
+    Epic: 3,
+    Legendary: 4,
+    Apex: 5
+};
+const CarWeightsConfig = [
+    {rarity: CarRarities.Common,weight: 1},
+    {rarity: CarRarities.Rare,weight: 10},
+    {rarity: CarRarities.Epic,weight: 50},
+    {rarity: CarRarities.Legendary,weight: 100},
+    {rarity: CarRarities.Apex,weight: 500},
+];
+
+module.exports = async (deployer, network, accounts) => {
+
+    switch(network){
+        case "ganache":
+            await deployer.deploy(AssetsInventoryMock,NFCollectionMaskLength);
+            this.nftContract = await AssetsInventoryMock.deployed();
+            await deployer.deploy(ERC20FullMock,DividendTokenInitialBalance);
+            this.dividendTokenContract = await ERC20FullMock.deployed();
+            break;
+        case "rinkeby":
+            const nftContractAddressRinkeby = program.nftContractAddressRinkeby;
+            const ERC20BaseAddressRinkeby = program.ERC20BaseAddressRinkeby;
+            this.nftContract = nftContractAddressRinkeby?await AssetsInventoryMock.at(nftContractAddressRinkeby):await AssetsInventoryMock.new(NFCollectionMaskLength);
+            this.dividendTokenContract = ERC20BaseAddressRinkeby? await ERC20FullMock.at(ERC20BaseAddressRinkeby):await ERC20FullMock.new(DividendTokenInitialBalance);
+            break;
+        case "mainnet":
+            
+            break;
+        default:
+            console.log(`Unknown network '${network}', stopping...`);
+            return;
+
+    }
+    
+    console.log("Creating Staking Contract");
+        
+    console.log("Deploying Staking Contract");
+    const result = await deployer.deploy(NftStakingMock,
+        PayoutPeriodLength,
+        FreezePeriodSeconds,
+        this.nftContract.address,
+        this.dividendTokenContract.address,
+        CarWeightsConfig.map(x => x.rarity),
+        CarWeightsConfig.map(x => x.weight));
+
+    this.stakingContract = await NftStakingMock.deployed();
+
+    await this.dividendTokenContract.transfer(this.stakingContract.address, DividendTokenInitialBalance);    
+    
+}

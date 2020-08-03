531b62edd624c22b194080759d4885a7f67c200e
Nathan Sala <nathans@animocabrands.com>
2020-08-03 12:25:24 +0800

    Version 3.0.2 (#10)
    Fixed an exploit that allowed the claiming of rewards for the cycle in which a token was unstaked.
    
    Co-authored-by: Jan-Paul Azucena <jp@fuelpowered.com>

---
+++ test/contracts/staking/scenarios/EarlyRestake.scenario.js
@@ -0,0 +1,42 @@
+const {
+    shouldRevertAndNotStakeNft,
+    shouldStakeNft,
+    shouldTimeWarpBy,
+    shouldUnstakeNft,
+    initialiseDebug,
+} = require('../behaviors');
+
+const {TokenIds} = require('../constants');
+
+const earlyRestakeScenario = function (staker) {
+    before(function () {
+        initialiseDebug.bind(this)(staker);
+    });
+
+    describe("when staking an NFT for the first time", function () {
+        shouldStakeNft(staker, TokenIds[0]);
+    });
+
+    describe('when unstaking an NFT after the freeze period', function () {
+        shouldTimeWarpBy({cycles: 2}, {cycle: 3});
+        shouldUnstakeNft(staker, TokenIds[0]);
+    });
+
+    describe('when re-staking an NFT in the same cycle it was unstaked', function () {
+        shouldRevertAndNotStakeNft(staker, TokenIds[0], 'NftStaking: unstaked token cooldown');
+    });
+
+    describe('when waiting 1 cycle before trying to re-stake', function () {
+        shouldTimeWarpBy({cycles: 1}, {cycle: 4});
+        shouldRevertAndNotStakeNft(staker, TokenIds[0], 'NftStaking: unstaked token cooldown');
+    });
+
+    describe('when waiting another cycle before trying to re-stake', function () {
+        shouldTimeWarpBy({cycles: 1}, {cycle: 5});
+        shouldStakeNft(staker, TokenIds[0]);
+    });
+};
+
+module.exports = {
+    earlyRestakeScenario,
+};
